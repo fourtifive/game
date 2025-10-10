7ee103742fa4371f178038371ac1492b5425af65
fourtifive <shishishishi41@qq.com>
2025-10-10 23:21:33 +0800

    11

--- include/core/InuputManager.h
+++ include/core/InuputManager.h
@@ -1,15 +1,31 @@
 #pragma once
 #include<glfw3.h>
+#include<unordered_set>
+#include<unordered_map>
 
+struct Input_State
+{
+	bool is_press=0;
+	float duration = 0.0f;
+};
 
 class InputManager
 {
 public:
 	InputManager()=default; 
-	~InputManager();
+	~InputManager()=default;
 
 	bool Init();
 
+
+	static void key_callback(GLFWwindow* window);
+	static void cursor_callback(GLFWwindow* window);
+
+
+
 private:
+	std::unordered_set<int> cursor;
+	std::unordered_map<int, Input_State> key;
+	//corsor's position;
 
 };
--- src/core/InuputManager.cpp
+++ src/core/InuputManager.cpp
@@ -4,3 +4,11 @@ bool InputManager::Init()
 {
     
 }
+
+void InputManager::key_callback(GLFWwindow* window)
+{
+}
+
+void InputManager::cursor_callback(GLFWwindow* window)
+{
+}
